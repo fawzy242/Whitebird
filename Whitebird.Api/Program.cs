5dfdabbd56264f1b12dd038d28428991695978ad
Fawzy <fawzy.imam2@gmail.com>
2025-10-13 19:10:28 +0700

    Initial commit - push source project

---
+++ Whitebird.Api/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Quantix.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Force TLS 1.2 (opsional, biasanya sudah default di .NET 6+)
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // Accept all SSL certificates (⚠️ jangan dipakai di production kalau bisa)
+            ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, certificate, chain, sslPolicyErrors) => true;
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
