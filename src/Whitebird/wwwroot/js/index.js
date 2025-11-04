ee7eed7d559d353d9be15b702b580c7deafd571d
Fawzy <fawzy.imam2@gmail.com>
2025-11-04 13:12:14 +0700

    push

--- src/Whitebird/wwwroot/js/index.js
+++ src/Whitebird/wwwroot/js/index.js
@@ -5,10 +5,10 @@ function loadPage(pageUrl) {
 
 // Saat document ready, load halaman login
 $(document).ready(function() {
-    loadPage('/public/Product/Features/login/Login.html'); // path menyesuaikan folder public
+    loadPage('../../Features/login/Login.html'); // path menyesuaikan folder public
 });
 
 // Contoh fungsi untuk pindah ke home
 function goToHome() {
-    loadPage('/public/Product/Features/home/Home.html');
+    loadPage('../../Features/home/Home.html');
 }
